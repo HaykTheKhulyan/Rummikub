de60e58f07cd2be072d5da37d48a1f71c85c086b
Hayk Khulyan <hkhulyan@gmail.com>
2020-01-04 21:34:49 -0800

    Added existing project files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    List<Tile> tiles = new List<Tile>();
+
+    GameObject player1;
+    GameObject player2;
+
+
+    void Start()
+    {
+        for (int val = 1; val <= 13; val++) {
+            for (int color = 1; color <= 4; color++) {
+                tiles.Add(new Tile(val, color));
+                tiles.Add(new Tile(val, color));
+            }
+        }
+        
+        Shuffle(tiles);
+
+        player1 = GameObject.Find("Player (1)");
+        player2 = GameObject.Find("Player (2)");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public Tile DrawTile() {
+        // picks index to remove from
+        int index = Random.Range(0, tiles.Count - 1);
+        // saves tile at that index
+        Tile temp = tiles[index];
+        // removes tile from list
+        tiles.RemoveAt(index);
+        // returns tile
+        return temp;
+    }
+
+
+
+
+    // helper methods
+
+    private Color getColorFromNumber(int num) {
+        switch (num) {
+            case 1:
+                return Color.blue;
+            case 2:
+                return Color.red;
+            case 3:
+                return Color.green;
+            default:
+                return Color.black;
+        }
+    }
+
+    private void Shuffle<T>(List<T> list)  
+    {  
+        for (int i = 0; i < list.Count; i++) {
+            T temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1: blue
+// 2: red
+// 3: green
+// 4: black
+
+// value goes from 1 to 13
+
+public class Tile : MonoBehaviour
+{
+    // public GameObject TileBlock;
+    private int value;
+    private int color;
+
+    public Tile(int _val, int _col) {
+        value = _val;
+        color = _col;
+    }
+
+    public int getValue() { return value; }
+    public int getColorAsInt() { return color; }
+    public string getColorAsString() { 
+        switch (color) {
+            case 1:
+                return "Blue";
+            case 2:
+                return "Red";
+            case 3:
+                return "Green";
+            default:
+                return "Black";
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
